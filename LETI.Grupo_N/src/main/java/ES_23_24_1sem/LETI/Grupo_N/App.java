28f56c30c5e0f47b7b417802a174cae22b91a093
Joao <joaolpmacedo@gmail.com>
2023-10-22 21:35:26 +0100

    User can now read local or remote schedules

--- LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/App.java
+++ LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/App.java
@@ -1,40 +1,64 @@
 package ES_23_24_1sem.LETI.Grupo_N;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-   
-	public void read(File file) {
-		
-		
-		String splitBy = ";";
-		File f = new File("HorarioDeExemplo.CSV");
-		
+public class App {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Do you want to read local or remote archives? (l/r): ");
+		String option = scanner.nextLine();
+
+		switch (option) {
+
+		case "l":
+			System.out.println("Type the path for local archive: ");
+			String path = scanner.nextLine();
+			readLocalArchive(path);
+			break;
+			
+		case "r":
+			System.out.println("Type the URL for remote archive: ");
+			String url = scanner.nextLine();
+			readRemoteArchive(url);
+			break;
+
+		default:
+			System.out.println("Invalid Option");
+			break;
+		}
+		scanner.close();
+	}
+
+	public static void readLocalArchive(String path) {
 		try {
-			
-			Scanner sc = new Scanner(f);
-			
-			
-		} catch (FileNotFoundException e) {
+			Scanner scanner = new Scanner(new File(path));
+			while (scanner.hasNextLine()) {
+				System.out.println(scanner.nextLine());
+			}
+			scanner.close();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	
-		
 	}
-	
-	
-	
-	
-	
-	public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void readRemoteArchive(String urlStr) {
+		try {
+			URL url = new URL(urlStr);
+			InputStream in = url.openStream();
+			Scanner scanner = new Scanner(in);
+			while (scanner.hasNextLine()) {
+				System.out.println(scanner.nextLine());
+			}
+			scanner.close();
+			in.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
