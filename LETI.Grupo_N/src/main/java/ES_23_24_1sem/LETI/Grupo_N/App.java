446259a27fc6f1a47530b049363d8d3f0e093e11
Pedro Duarte <pedroAEDuarte03@gmail.com>
2023-10-22 15:59:32 +0100

    Project to eclipse and maven dependencies

---
+++ LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/App.java
@@ -0,0 +1,13 @@
+package ES_23_24_1sem.LETI.Grupo_N;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
