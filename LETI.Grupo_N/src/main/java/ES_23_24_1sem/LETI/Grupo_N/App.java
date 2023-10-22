417876a7fff4cf1a3324870f5e83a931fa22e0a4
Joao <joaolpmacedo@gmail.com>
2023-10-22 17:56:05 +0100

    First steps of read() function

--- LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/App.java
+++ LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/App.java
@@ -1,12 +1,39 @@
 package ES_23_24_1sem.LETI.Grupo_N;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+   
+	public void read(File file) {
+		
+		
+		String splitBy = ";";
+		File f = new File("HorarioDeExemplo.CSV");
+		
+		try {
+			
+			Scanner sc = new Scanner(f);
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	
+		
+	}
+	
+	
+	
+	
+	
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
