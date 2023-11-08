1671f4a98ac33cc29be47fcc36b5b2a4b9bd84b9
joao4real <joaolpmacedo@gmail.com>
2023-11-08 13:44:35 +0000

    Software can now store data from remote files too

--- LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/App.java
+++ LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/App.java
@@ -1,12 +1,15 @@
 package ES_23_24_1sem.LETI.Grupo_N;
 
-import java.io.InputStream;
-import java.net.URL;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
 	
 	public static void main(String[] args) {
+		
+		Schedule schedule = new Schedule();
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Do you want to read local or remote archives? (l/r): ");
@@ -15,45 +18,31 @@ public class App {
 
 		case "l":
 			System.out.println("Type the path for local archive: ");
-			Schedule schedule = Schedule.createScheduleByLocalFile(scanner.nextLine());
-			System.out.println(schedule.map.get("Unidade Curricular"));
+			schedule = Schedule.createScheduleByLocalFile(scanner.nextLine());
+			System.out.println(schedule.getMap().get("Unidade Curricular"));
 			break;
 
 		case "r":
 			System.out.println("Type the URL for remote archive: ");
-			readRemoteArchive(scanner.nextLine());
+			schedule = Schedule.createScheduleByRemoteFile(scanner.nextLine());
+			System.out.println(schedule.getMap().get("Unidade Curricular"));
 			break;
 
 		default:
 			System.out.println("Invalid Option");
 			break;
 		}
+		
 		scanner.close();
-	}
-
-	/*private File getLocalArchive(String path) {
-		return new File(path);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}*/
-
-	public static void readRemoteArchive(String urlStr) {
+		
 		try {
-			InputStream in = new URL(urlStr).openStream();
-			Scanner scanner = new Scanner(in);
-			while (scanner.hasNextLine()) {
-				System.out.println(scanner.nextLine());
-			}
-			scanner.close();
-			in.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		    FileWriter fw = new FileWriter("SalasDeAulaPorTipoDeSala.html");
+		    fw.write("MCPILADEAÇO");
+		    System.out.println("Passei por aqui");
+		    fw.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
 		}
-	} 
 		
-		
-		
-		
+	}
 }
--- LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/Schedule.java
+++ LETI.Grupo_N/src/main/java/ES_23_24_1sem/LETI/Grupo_N/Schedule.java
@@ -2,6 +2,10 @@ package ES_23_24_1sem.LETI.Grupo_N;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -9,56 +13,85 @@ import java.util.Scanner;
 
 public class Schedule {
 
-	public LinkedHashMap<String, List<String>> map = new LinkedHashMap<>();
+	private LinkedHashMap<String, List<String>> map = new LinkedHashMap<>();
 
-	private Schedule() {
+	public Schedule() {
 	}
 
 	public static Schedule createScheduleByLocalFile(String path) {
-
+		
 		Schedule schedule = new Schedule();
-
+		
 		try {
+			
 			Scanner sc = new Scanner(new File(path));
-			String line = sc.nextLine();
-			String[] labels = line.split(";");
-
-			for (int i = 0; i < labels.length; i++)
-				schedule.map.put(labels[i], new ArrayList<String>());
-
-			while (sc.hasNextLine())
-				schedule.addInfoByLine(sc.nextLine());
-
-			sc.close();
+			schedule.readFile(sc);
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-
+		
 		return schedule;
 	}
+	
+	public static Schedule createScheduleByRemoteFile(String urlStr) {
+		
+		Schedule schedule = new Schedule();
+		
+		try {
+			
+			InputStream in = new URL(urlStr).openStream();
+			schedule.readFile(new Scanner(in));
+			in.close();
+			
+		} catch (MalformedURLException e) {
+	        System.err.println("Invalid URL: " + urlStr);
+	        e.printStackTrace();
+	        
+	    } catch (IOException e) {
+	        System.err.println("Failed to open the remote file: " + urlStr);
+	        e.printStackTrace();
+	    }
+		
+		return schedule;
+}
 
 	private void addInfoByLine(String line) {
 
 		String[] data = my_split(line);
-		int i = 0; 
-		for (String key : map.keySet()) 
-			map.get(key).add(data[i++]);
+		int i = 0;
+		for (String key : getMap().keySet())
+			getMap().get(key).add(data[i++]);
 	}
-		
-		
+
 	private static String[] my_split(String line) {
-        String[] arr = new String[11];
-        int i = 0;
-        int s = 0;
-        int f = 0;
-        for (int j = 0; j < line.length(); j++) {
-            if (line.charAt(j) == ';') {
-                arr[i++] = (s == f) ? "N/A" : line.substring(s, f);
-                s = f + 1;
-            }
-            f++;
-        }
-        arr[i] = (s == f) ? "N/A" : line.substring(s, f);		
+		String[] arr = new String[11];
+		int i = 0;
+		int s = 0;
+		int f = 0;
+		for (int j = 0; j < line.length(); j++) {
+			if (line.charAt(j) == ';') {
+				arr[i++] = (s == f) ? "N/A" : line.substring(s, f);
+				s = f + 1;
+			}
+			f++;
+		}
+		arr[i] = (s == f) ? "N/A" : line.substring(s, f);
 		return arr;
+	}
+
+	public void readFile(Scanner sc) {
+		String line = sc.nextLine();
+		String[] labels = line.split(";");
+
+		for (int i = 0; i < labels.length; i++)
+			getMap().put(labels[i], new ArrayList<String>());
+
+		while (sc.hasNextLine())
+			addInfoByLine(sc.nextLine());
+	}
+
+	public LinkedHashMap<String, List<String>> getMap() {
+		return map;
 	}
 }
